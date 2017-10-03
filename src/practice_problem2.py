d38dd86f921b6aea9d2cfa473aba3a5694269f23
grunindm <grunindm@rose-hulman.edu>
2017-10-03 13:17:13 -0400

    completely all problems except p1

--- src/practice_problem2.py
+++ src/practice_problem2.py
@@ -196,10 +196,11 @@ def practice_problem2b(sequence):
     """
     seq = ''
     for k in range(len(sequence)):
-        seq += str(sequence[k])
+        if (len(sequence[k]) != 0):
+            seq += sequence[k][0]
     return seq
     ####################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
